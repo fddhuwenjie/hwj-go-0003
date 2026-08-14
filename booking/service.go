b42626dc4cfc99bbd551bddbc7205a3fa9028964
huwenjie <huwenjie@users.noreply.github.com>
2026-08-15 00:54:14 +0800

    Compare booking intervals by absolute instant

--- booking/service.go
+++ booking/service.go
@@ -277,9 +277,7 @@ func (s *Service) Utilization(req UtilizationRequest) (UtilizationResponse, erro
 // overlaps reports whether [s1,e1) and [s2,e2) share any time. Adjacent
 // intervals (e1 == s2 or e2 == s1) do not overlap.
 func overlaps(s1, e1, s2, e2 time.Time) bool {
-	const wallClockLayout = "2006-01-02T15:04:05.999999999"
-	return s1.Format(wallClockLayout) < e2.Format(wallClockLayout) &&
-		s2.Format(wallClockLayout) < e1.Format(wallClockLayout)
+	return s1.Before(e2) && s2.Before(e1)
 }
 
 // clampDuration returns the duration that [rStart, rEnd) overlaps
