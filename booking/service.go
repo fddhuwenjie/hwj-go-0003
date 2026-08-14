a28fd904068072b5aa38d9a93c79f6b5fb73edba
huwenjie <huwenjie@users.noreply.github.com>
2026-08-15 00:51:41 +0800

    Introduce cross-timezone booking conflict regression

--- booking/service.go
+++ booking/service.go
@@ -277,7 +277,9 @@ func (s *Service) Utilization(req UtilizationRequest) (UtilizationResponse, erro
 // overlaps reports whether [s1,e1) and [s2,e2) share any time. Adjacent
 // intervals (e1 == s2 or e2 == s1) do not overlap.
 func overlaps(s1, e1, s2, e2 time.Time) bool {
-	return s1.Before(e2) && s2.Before(e1)
+	const wallClockLayout = "2006-01-02T15:04:05.999999999"
+	return s1.Format(wallClockLayout) < e2.Format(wallClockLayout) &&
+		s2.Format(wallClockLayout) < e1.Format(wallClockLayout)
 }
 
 // clampDuration returns the duration that [rStart, rEnd) overlaps
